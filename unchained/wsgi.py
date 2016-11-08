11635c8f7f444b7550477f8974394c8158bd4bdb
Preston D. Badeer <prrstn@gmail.com>
2016-11-08 09:21:09 -0600

    WSGI critical fix, virtual environment includes
    The django.core.wsgi import location was breaking apache, moved it to
    the proper line. Added a lot of deployment prep here to assist with
    deploying using a virtual environment on an AWS Ubuntu EC2 server.

--- unchained/wsgi.py
+++ unchained/wsgi.py
@@ -1,6 +1,22 @@
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+import site
 
+# Add virtual environment packages to site directory
+site.addsitedir('~/.virtualenvs/unchained/local/lib/python2.7/site-packages')
+
+# Add project path to the sys.path
+sys.path.append('/var/www/html/')
+
+# Import Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unchained.settings")
+
+# Activate virtual environment
+activate_env = os.path.expanduser(
+    "/home/ubuntu/.virtualenvs/unchained/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+
+# Don't move this import!
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
